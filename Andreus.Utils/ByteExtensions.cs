dc6ec500694deb3b524331fd7c7678715fb3acd2
Andreus <andreus_emperor@mail.ru>
2020-09-27 01:04:14 +0700

    Start

---
+++ Andreus.Utils/ByteExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Andreus.Utils
+{
+    public static class ByteExtensions
+    {
+        public static string ToStringInvariant(this byte value)
+        {
+            return value.ToString(NumberFormatInfo.InvariantInfo);
+        }
+
+        public static string ToStringInvariant(this byte value, string format)
+        {
+            return value.ToString(format, NumberFormatInfo.InvariantInfo);
+        }
+    }
+}
