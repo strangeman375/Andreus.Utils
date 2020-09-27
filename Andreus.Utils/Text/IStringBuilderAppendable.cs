5543dc5654f527d1aa22c454b0d55ddc63063f9e
Andreus <andreus_emperor@mail.ru>
2020-09-27 23:02:53 +0700

    Add docs

--- Andreus.Utils/Text/IStringBuilderAppendable.cs
+++ Andreus.Utils/Text/IStringBuilderAppendable.cs
@@ -2,8 +2,15 @@
 
 namespace Andreus.Utils.Text
 {
+    /// <summary>
+    /// Provides functionality to append the value of an object to a <see cref="StringBuilder"/>.
+    /// </summary>
     public interface IStringBuilderAppendable
     {
+        /// <summary>
+        /// Appends the value of the current instance to a specified <see cref="StringBuilder"/>.
+        /// </summary>
+        /// <param name="stringBuilder">The <see cref="StringBuilder"/> to append to.</param>
         void AppendSelf(StringBuilder stringBuilder);
     }
 }
--- Andreus.Utils/Text/StringBuilderExtensions.cs
+++ Andreus.Utils/Text/StringBuilderExtensions.cs
@@ -9,6 +9,13 @@ namespace Andreus.Utils.Text
     /// </summary>
     public static class StringBuilderExtensions
     {
+        /// <summary>
+        /// Appends the string representation of a specified object to this instance.
+        /// </summary>
+        /// <typeparam name="T">The type of a specified object.</typeparam>
+        /// <param name="stringBuilder">The <see cref="StringBuilder"/> to append to.</param>
+        /// <param name="value">The object to append.</param>
+        /// <returns>A reference to this instance after the append operation has completed.</returns>
         public static StringBuilder AppendValue<T>(this StringBuilder stringBuilder, T value)
             where T : IStringBuilderAppendable
         {
@@ -19,6 +26,14 @@ namespace Andreus.Utils.Text
             return stringBuilder;
         }
 
+        /// <summary>
+        /// Concatenates and appends the members of a collection, using the specified <see cref="string"/> separator between each member.
+        /// </summary>
+        /// <typeparam name="T">The type of the members of values.</typeparam>
+        /// <param name="stringBuilder">The <see cref="StringBuilder"/> to append to.</param>
+        /// <param name="separator">The string to use as a separator.</param>
+        /// <param name="values">A collection that contains the objects to concatenate and append to the current instance of the <see cref="StringBuilder"/>.</param>
+        /// <returns>A reference to this instance after the append operation has completed.</returns>
         public static StringBuilder AppendJoinValues<T>(this StringBuilder stringBuilder, string separator, IEnumerable<T> values)
             where T : IStringBuilderAppendable
         {
@@ -45,6 +60,14 @@ namespace Andreus.Utils.Text
             return stringBuilder;
         }
 
+        /// <summary>
+        /// Concatenates and appends the members of a collection, using the specified <see cref="char"/> separator between each member.
+        /// </summary>
+        /// <typeparam name="T">The type of the members of values.</typeparam>
+        /// <param name="stringBuilder">The <see cref="StringBuilder"/> to append to.</param>
+        /// <param name="separator">The char to use as a separator.</param>
+        /// <param name="values">A collection that contains the objects to concatenate and append to the current instance of the <see cref="StringBuilder"/>.</param>
+        /// <returns>A reference to this instance after the append operation has completed.</returns>
         public static StringBuilder AppendJoinValues<T>(this StringBuilder stringBuilder, char separator, IEnumerable<T> values)
             where T : IStringBuilderAppendable
         {
